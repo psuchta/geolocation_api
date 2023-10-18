347cc6ba338dd948aece72f27067ab0eeffc145f
Pawel Suchta <pawel.suchta@polcode.net>
2023-10-18 16:12:39 +0200

    Add key for ipstack

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module GeolocationApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    ENV['IPSTACK_ACCESS_KEY'] = Rails.application.credentials.dig(:ipstack_access_key)
 
     # Configuration for the application, engines, and railties goes here.
     #
