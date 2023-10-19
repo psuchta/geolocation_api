14ff932c4cadf9b6603e7687a2e5a21a7648f33b
Pawel Suchta <pawel.suchta@polcode.net>
2023-10-19 19:28:07 +0200

    Add controller and logic for endpoints

---
+++ app/controllers/api/v1/ip_infos_controller.rb
@@ -0,0 +1,36 @@
+class Api::V1::IpInfosController < ApplicationController
+  before_action :set_ip, only: [:show, :destroy]
+
+  def show
+    render json: { data: @ip }, status: :ok
+  end
+
+  def create
+    #  It wasn't sepcified in the task description, but I assumed that if we try to create an ip which is already in the app's database,
+    #  a new geodata will be fetched from the ipstack endpoint and it will replace the old geodata related to that ip.
+
+    render json: { data: {} }, status: :ok
+  end
+
+  def destroy
+    if @ip.destroy
+      render json: { message: 'Destroy Success', data: @ip }, status: :ok
+    else
+      render json: { message: @ip.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_ip
+    param_ip = ip_search_attributes[:ip]
+    @ip = IpInfo.where(ip: param_ip)
+                .or(IpInfo.where(url: param_ip)).first
+
+    raise ActiveRecord::RecordNotFound unless @ip
+  end
+
+  def ip_search_attributes
+    params.permit(:ip)
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+
+  rescue_from ActiveRecord::RecordNotFound do
+    render json: { message: "Requested Ip cannot be found in the database" },
+           status: :not_found
+  end
 end
