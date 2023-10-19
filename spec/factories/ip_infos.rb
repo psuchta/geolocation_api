6cdf6d6d028fd9fedc708ba757279fee22d2c9b9
Pawel Suchta <pawel.suchta@polcode.net>
2023-10-19 21:08:02 +0200

    Add basic tests for POST in ip_infos_controller

--- spec/factories/ip_infos.rb
+++ spec/factories/ip_infos.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :ip_info do
-    ip { "MyString" }
-    url { "MyString" }
+    ip { '8.8.8.8' }
+    url { 'www.google.pl' }
     ip_type { :ipv4 }
-    continent_name { "Europe" }
-    country_name { "Poland" }
-    region_name { "Mazovia" }
-    city { "Ostrów Mazowiecka" }
-    zip { "07-300" }
+    continent_name { 'Europe' }
+    country_name { 'Poland' }
+    region_name { 'Mazovia' }
+    city { 'Ostrów Mazowiecka' }
+    zip { '07-300' }
     latitude { 52.80529022216797 }
     longitude { 21.8941707611084 }
   end
--- spec/requests/ip_infos_spec.rb
+++ spec/requests/ip_infos_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe 'IpInfos', type: :request do
           'latitude' => '52.80529022216797',
           'longitude' => '21.8941707611084',
           'region_name' => 'Mazovia',
-          'url' => 'MyString',
+          'url' => 'www.google.pl',
           'zip' => '07-300'
         }
         get '/api/v1/ip_infos', params: { ip: '8.8.8.8' }
@@ -65,6 +65,47 @@ RSpec.describe 'IpInfos', type: :request do
 
         expect(response).to have_http_status(404)
         expect(json['message']).to eq('Requested Ip cannot be found in the database')
+      end
+    end
+  end
+
+  describe 'POST /create' do
+    let!(:ip_info) { FactoryBot.create(:ip_info, ip: '8.8.8.8') }
+
+    it 'returns status code 200', :vcr do
+      post '/api/v1/ip_infos', params: { ip: '7.7.7.7', latitude: 12.12 }
+
+      expect(response).to have_http_status(200)
+    end
+
+    context 'when ip is in the database' do
+      it 'updates IP with new geodata' do
+        post '/api/v1/ip_infos', params: { ip: '7.7.7.7' }
+
+        expect(json['data']['latitude']).to eq('39.043701171875')
+        expect(ip_info.reload.latitude).to eq('39.043701171875')
+      end
+    end
+
+    context 'when ip is not in the database' do
+      it 'creates new ip in the database' do
+        expected = {
+          'city' => 'Ashburn',
+          'continent_name' => 'North America',
+          'country_name' => 'United States',
+          'ip' => '7.7.7.7',
+          'ip_type' => 'ipv4',
+          'latitude' => '39.043701171875',
+          'longitude' => '-77.47419738769531',
+          'region_name' => 'Virginia',
+          'url' => 'www.google.pl',
+          'zip' => '20147'
+        }
+
+        expect { post '/api/v1/ip_infos', params: { ip: '7.7.7.7' } }
+          .to change { IpInfo.count }.by(1)
+
+        expect(json).to eq(expected)
       end
     end
   end
